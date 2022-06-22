1e483494457fcb2022644800220c664e5ea113cc
NicoleG <guillen.nicole@pucp.edu.pe>
2022-06-21 23:10:12 -0500

    parte1

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
 $(document).ready(function () {
+    let url = "https://pokeapi.co/api/v2/region"
+    $.ajax({
+        method: "GET",
+        url: url,
+    }).done(function (data){
+        let listapokemones = data.results;
+        let tabla = "";
+        for (let i = 0; i < listapokemones.length; i++) {
+            tabla += "<tr>";
+            tabla += "<td>" + (i + 1) + "</td>";
+            tabla += "<td>"+ listapokemones[i].name + "</td>";
+            tabla += "<td><a href='detalleRegion/detalleRegion.html?region="+(i+1)+"' class='btn btn-primary botonDetalle'>" + "Detalle" + "</a></td>";
+            tabla += "</tr>";
+        }
 
+        $("#body-paises").html(tabla);
+    }).fail(function (e){
+        console.log(e)
+    })
 
 });
